5c5c862ee895b3404771bbad5d0e5beb5bfb3ad0
Stephen Sawchuk <sawchuk@gmail.com>
2015-02-23 15:07:34 -0500

    this looks right, right?

---
+++ index.js
@@ -0,0 +1,14 @@
+var secs = module.exports.secs = module.exports.seconds =
+function (s) { return s * 1000 }
+
+var mins = module.exports.mins = module.exports.minutes =
+function (m) { return m * secs(60) }
+
+var hrs = module.exports.hrs = module.exports.hours =
+function (h) { return h * m(60) }
+
+var days = module.exports.days =
+function (d) { return d * hrs(24) }
+
+var weeks = module.exports.days =
+function (w) { return w * days(7) }
